9eade4633ad914adb9e69bc35b3d0d4ca31826d4
tuanda <adang-ext@moethennessy.com>
2025-08-29 13:02:09 +0700

    update

--- services/orderService/index.js
+++ services/orderService/index.js
@@ -55,7 +55,7 @@ const formatDiamondOrder = async ({ order }) => {
         country: order.shipping_address?.country || "",
         contact_name: order.shipping_address?.name || "",
         telephone_number: order.shipping_address?.phone || "",
-        deliverToCompanyAddress: false
+        deliverToCompanyAddress: true
       }
     }
   }
@@ -86,7 +86,7 @@ const formatHendiOrder = ({ order }) => {
         country: order.shipping_address?.country || "",
         contact_name: order.shipping_address?.name || "",
         telephone_number: order.shipping_address?.phone || "",
-        deliverToCompanyAddress: false
+        deliverToCompanyAddress: true
       }
     }
   }
